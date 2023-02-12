fbdcdfa2d621319cf71f84e1c4407d4db44a2d1e
MinyShrimp <ksk7584@gmail.com>
2023-02-13 05:32:25 +0900

    1강: 타임리프 - 기본 기능 - 프로젝트 생성

---
+++ src/test/java/hello/springcoremvc21/SpringCoreMvc21ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springcoremvc21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreMvc21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
